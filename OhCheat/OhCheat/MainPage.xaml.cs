a7178606a15ad948ae4c23d7c19e9b6f7e9531ed
Eduardo G. Rodrigues <49928136+EscaFritz@users.noreply.github.com>
2019-11-18 20:34:36 -0300

    Alteração Eduardo

--- OhCheat/OhCheat/MainPage.xaml.cs
+++ OhCheat/OhCheat/MainPage.xaml.cs
@@ -14,6 +14,7 @@ namespace OhCheat
             InitializeComponent();
             //UMA ALTERAÇÃO CABULOSA
             //OUTRA ALTERAÇÃO CABULOSA
+            //Altração Senninha
         }
     }
 }
